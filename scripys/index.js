a058654734f7e8fad9129536a1dd39e92f024f06
JvitorJ <joaovictorresende59@gmail.com>
2022-10-21 22:11:57 -0300

    commit 2

--- scripys/index.js
+++ scripys/index.js
@@ -1,3 +1,25 @@
+let gamer = "X"
+
+function winner(){
+    let elements = document.getElementBytd("tabuleiro").children;
+    if(
+        elements[0].innerText === elements[1].innerText &&
+        elements[1].innerText === elements[2].innerText &&
+        elements[0].innerText.length !== 0
+    ) {
+        elements[0].style="background-color: red;";
+    }
+
+}
+
 function mark(element) {
-    element.innerText = "x"
+    if (element.innerText.length === 0) {
+        element.innerText = gamer;
+        winner
+        if (gamer === "X") {
+            gamer = "O"
+        } else {
+            gamer = "X"
+        }
+    }
 }
